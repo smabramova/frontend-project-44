77e096a1136ef872328904d027fc5bc96abb243d
Светлана <144787642+smabramova@users.noreply.github.com>
2024-06-10 08:43:50 +0000

    ..

--- src/getRandomInteger.js
+++ src/getRandomInteger.js
@@ -1,5 +1,5 @@
 export default (min, max) => {
-    const tempMin = Math.ceil(min);
-    const tempMax = Math.floor(max);
-    return Math.floor(Math.random() * (tempMax - tempMin + 1)) + tempMin;
-  };
+  const tempMin = Math.ceil(min);
+  const tempMax = Math.floor(max);
+  return Math.floor(Math.random() * (tempMax - tempMin + 1)) + tempMin;
+};
